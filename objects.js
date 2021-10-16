848ec63bf8f31afb3a61cbdb0983adee8098efc4
Denis Yeresko <denis.yeresko@gmail.com>
2021-10-16 13:27:28 +0300

    modified files

---
+++ objects.js
@@ -0,0 +1,25 @@
+function makeUser(name, age)
+{
+    return {
+        name,
+        age
+    }
+}
+
+// let user = new Object(); // синтаксис "конструктор объекта"
+// let user = {}; // синтаксис "литерал объекта"
+// let user = {firstname: "John", age: 30};
+// alert(user.firstname);
+// alert(user.age);
+// user.isAdmin = true;
+// user.age = 31;
+// alert(user.isAdmin);
+// alert(user.age);
+// user["likes birds"] = true;
+// alert(user["likes birds"]);
+// delete user["likes birds"];
+// let fruit = prompt("Какой фрукт купить?", "apple");
+// let bag = {[fruit]: 5};
+// alert(bag[[fruit]]);
+let user = makeUser("John", 30);
+alert(user.name);
